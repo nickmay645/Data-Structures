fb6ea49e06ed6c919dd589c3cbc886ca4838de48
Nick <nickmay645@gmail.com>
2021-08-02 11:38:29 -0400

    stack peek and queue init

---
+++ queue.py
@@ -0,0 +1,31 @@
+"""
+A queue is a useful data structure in programming. It is similar to the ticket queue outside a cinema hall,
+where the first person entering the queue is the first person who gets the ticket.
+
+Queue follows the First In First Out (FIFO) rule - the item that goes in first is the item that comes out first.
+"""
+
+
+# Queue implementation in Python
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
--- stack.py
+++ stack.py
@@ -33,6 +33,28 @@ def peek(stack):
 
 
 """
+A stack is a linear data structure that follows the principle of Last In First Out (LIFO). 
+This means the last element inserted inside the stack is removed first.
+
 Stack Time Complexity
+---------------------
 For the array-based implementation of a stack, the push and pop operations take constant time, i.e. O(1).
+
+========================================================================================================================
+Applications of Stack Data Structure
+------------------------------------
+Although stack is a simple data structure to implement, it is very powerful. The most common uses of a stack are:
+
+To reverse a word - Put all the letters in a stack and pop them out. Because of the LIFO order of stack, you will get 
+the letters in reverse order. 
+
+In compilers - Compilers use the stack to calculate the value of expressions like 2 + 4 / 5 * (7 - 9) by converting the 
+expression to prefix or postfix form. 
+
+In browsers - The back button in a browser 
+saves all the URLs you have visited previously in a stack. Each time you visit a new page, it is added on top of the 
+stack. When you press the back button, the current URL is removed from the stack, and the previous URL is accessed. 
+
+========================================================================================================================
 """
+
